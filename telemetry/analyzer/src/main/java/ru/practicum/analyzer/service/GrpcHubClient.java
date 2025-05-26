647aa7afd437aaae2a414eb8d5a4bd4e0d0fc057
Boris <Zelyanin>
2025-05-26 22:46:59 +0300

    create a new function

--- telemetry/analyzer/src/main/java/ru/practicum/analyzer/service/GrpcHubClient.java
+++ telemetry/analyzer/src/main/java/ru/practicum/analyzer/service/GrpcHubClient.java
@@ -4,13 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequest;
-import ru.yandex.practicum.grpc.telemetry.hubRouter.HubRouterControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc  ;
+
 
 @Slf4j
 @Component
 public class GrpcHubClient {
 
-    private final HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient;
+    private HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient;
 
     public GrpcHubClient(@GrpcClient("hub-router")
                          HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient) {
